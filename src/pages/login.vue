7fa851b848218f585677248209d5d113ed47edba
lnh <scjlnh@163.com>
2018-03-14 18:00:36 +0800

    添加登陆页

---
+++ src/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login_page">
+        这是一个登陆页
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+    .login_page{
+        width: 100%;
+        height: 100%;
+        background-color: #324057;
+    }
+</style>
+
