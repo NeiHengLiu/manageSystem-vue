79573586bbfe0993d09336dfc1e9a48a748a8705
lnh <scjlnh@163.com>
2018-03-15 13:43:31 +0800

    完成登陆页排版

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,16 +1,90 @@
 <template>
     <div class="login_page wah_100">
-        这是一个登陆页
+        <transition name="form-fade">
+            <section class="login_center" v-show="loginShow">
+                <div class="login_title">
+                    <p>外卖后台管理系统</p>
+                </div>
+                <el-form ref="loginFrom" :model="loginFrom">
+                    <el-form-item>
+                        <el-input v-model="loginFrom.userName" placeholder="用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input type="password" v-model="loginFrom.userPwd" placeholder="密码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" class="submit_btn">登陆</el-button>
+                    </el-form-item>
+                </el-form>
+                <p class="tip">温馨提示:</p>
+                <p class="tip">用户名为：xiaobai</p>
+                <p class="tip">密码为：123456</p>
+            </section>
+        </transition>
     </div>
 </template>
 
 <script>
-    
+    export default {
+        data () {
+            return {
+                loginFrom: {
+                    userName: '',
+                    userPwd: ''
+                },
+                loginShow: true
+            }
+        }
+    }
 </script>
 
 <style>
     .login_page{
         background-color: #324057;
+        position: relative;
     }
+    .login_page .login_center{
+        width: 370px;
+        height: 260px;
+        padding: 25px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-sizing: border-box;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin-top: -125px;
+        margin-left: -185px;
+    }
+    .login_page .login_title{
+        width: 100%;
+        position: absolute;
+        left: 0px;
+        top: -100px;
+    }
+    .login_page .login_title p{
+        font-size: 34px;
+        color: #fff;
+        text-align: center;
+    }
+    .login_page .submit_btn{
+        width: 100%;
+        font-size: 16px;
+    }
+    .login_page .tip{
+        color: red;
+        font-size: 12px;
+        text-align: center;
+        line-height: 1.1;
+    }
+    .form-fade-enter-active,
+    .form-fade-leave-active {
+	  	transition: all 1s;
+	}
+    .form-fade-enter,
+    .form-fade-leave-active {
+	  	transform: translate3d(0, -50px, 0);
+	  	opacity: 0;
+	}
 </style>
 
