a37ac0c892542d6120745a22abaf17d8e82762cc
lnh <scjlnh@163.com>
2018-03-21 13:03:51 +0800

    完成用户列表

--- src/pages/userList.vue
+++ src/pages/userList.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="user_list">
 		<head-top></head-top>
+		<div class="user_center">
+			<div class="user_table">
+				<el-table :data="tableData" header-row-class-name="user_list_table_head" style="width:100%;border:1px solid #ebeef5;">
+					<el-table-column type="index" :index="indexMethod" width="100"></el-table-column>
+					<el-table-column prop="date" label="注册日期" width="200"></el-table-column>
+					<el-table-column prop="name" label="用户姓名" width="200"></el-table-column>
+					<el-table-column prop="address" label="注册地址"></el-table-column>
+				</el-table>
+			</div>
+			<div class="page_number">
+				<el-pagination
+					@current-change="changePage"
+					layout="total,prev,pager,next"
+					:page-size="lineNumber"
+					:total="1000">
+				</el-pagination>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -9,7 +27,32 @@
 	export default {
 		data () {
 			return {
-				
+				tableData: [
+					{
+						date: '2018-03-21',
+						name: '小白',
+						address: '重庆市渝中区较场口日月光广场R1栋'
+					},
+					{
+						date: '2018-03-21',
+						name: '小白',
+						address: '重庆市渝中区较场口日月光广场R1栋'
+					},
+					{
+						date: '2018-03-21',
+						name: '小白',
+						address: '重庆市渝中区较场口日月光广场R1栋'
+					}
+				],
+				lineNumber: 20,		// 每页显示多少行
+			}
+		},
+		methods: {
+			indexMethod (index) {
+				return index + 1;
+			},
+			changePage (val) {
+				console.log(`当前页：${val}页`);
 			}
 		},
 		components: {
@@ -19,6 +62,24 @@
 </script>
 
 <style>
+	.user_list .user_center{
+		width: 100%;
+		padding: 20px 20px;
+		box-sizing: border-box;
+	}
 
+	.user_list .user_table{
+		width: 100%;
+	}
+
+	.user_list .page_number{
+		width: 100%;
+		margin-top: 15px;
+	}
+
+	.user_list .user_list_table_head th{
+		background-color: #eef1f6;
+		color: #1f2d3d;
+	}
 </style>
 
