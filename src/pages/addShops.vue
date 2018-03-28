e95e01f42f8da284cfcef5d0c92c20af0f4c3ab7
lnh <scjlnh@163.com>
2018-03-28 18:08:30 +0800

    添加店铺

--- src/pages/addShops.vue
+++ src/pages/addShops.vue
@@ -2,7 +2,21 @@
     <div class="add_shops">
         <head-top></head-top>
         <div class="baseList_table_conter">
-            
+            <el-form ref="addShopsForm" :model="addShopsForm" label-width="100px">
+                <el-form-item label="店铺名称">
+                    <el-input v-model="addShopsForm.shopName" placeholder="请输入店铺名称"></el-input>
+                </el-form-item>
+                <el-form-item label="详细地址">
+                    <el-autocomplete
+                        v-model="city"
+                        :fetch-suggestions="querySearch"
+                        :trigger-on-focus="false"
+                        @select="handleSelect"
+                        placeholder="请输入详细地址"
+                        >
+                    </el-autocomplete>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
@@ -12,7 +26,18 @@
     export default {
         data () {
             return {
+                addShopsForm: {
+                    shopName: ''
+                },
+                city: ''
+            }
+        },
+        methods: {
+            querySearch () {
 
+            },
+            handleSelect (item) {
+                console.log(item);
             }
         },
         components: {
