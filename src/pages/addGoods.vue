4fec0659dda581bd598f800bbb80087191d19bb1
lnh <scjlnh@163.com>
2018-04-04 17:58:28 +0800

    添加商品页

--- src/pages/addGoods.vue
+++ src/pages/addGoods.vue
@@ -2,8 +2,9 @@
     <div class="add_goods">
         <head-top></head-top>
         <div class="baseList_table_conter">
-            <el-form ref="addGoodsForm" :model="addGoodsForm" label-width="100px">
-                <el-form-item label="">
+            <header class="form_header">选择食品种类</header>
+            <el-form ref="addGoodsForm" :model="addGoodsForm" label-width="100px" class="add_goods_form">
+                <el-form-item label="食品种类">
 
                 </el-form-item>
             </el-form>
@@ -31,6 +32,14 @@
 </script>
 
 <style>
-
+    .add_goods_form:hover{
+        box-shadow: 0 0 8px 0 rgba(232,237,250,.6), 0 2px 4px 0 rgba(232,237,250,.5);
+        border-radius: 6px;
+        transition: all 400ms;
+    }
+    .form_header{
+		text-align: center;
+		margin-bottom: 10px;
+	}
 </style>
 
