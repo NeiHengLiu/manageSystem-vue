8e6b27cd09684e0772b59ef8a91d86d9465b50ac
lnh <scjlnh@163.com>
2018-04-23 16:54:41 +0800

    添加富文本编辑

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import vueQuillEditor from 'vue-quill-editor'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
+Vue.use(vueQuillEditor);
 
 new Vue({
 	el: '#app',
--- src/pages/richTextEditor.vue
+++ src/pages/richTextEditor.vue
@@ -2,21 +2,23 @@
     <div class="rich_text_editor">
         <head-top></head-top>
         <div class="baseList_table_conter">
-            123
+            <quill-editor v-model="content"></quill-editor>
         </div>
     </div>
 </template>
 
 <script>
     import headTop from '../components/headTop'
+    import { quillEditor } from 'vue-quill-editor'
     export default {
         data () {
             return {
-
+                content: ''
             }
         },
         components: {
-            headTop
+            headTop,
+            quillEditor
         }
     }
 </script>
